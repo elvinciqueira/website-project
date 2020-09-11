ef07ac4860b38c1a9a8338a87770eefdcee48eec
elvinciqueira <elvinciqueira@gmail.com>
2020-09-11 16:16:53 -0300

    Page transitions

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let pageScene;
 const mouse = document.querySelector('.cursor');
 const mouseText = mouse.querySelector('span');
 const burger = document.querySelector('.burger');
+const logo = document.querySelector('#logo');
 
 function animateSlides() {
   //Init Controller
@@ -114,9 +115,67 @@ function navToggle(event) {
   }
 }
 
+barba.init({
+  views: [
+    {
+      namespace: 'home',
+      beforeEnter() {
+        animateSlides();
+        logo.href = './index.html';
+      },
+      beforeLeave() {
+        slideScene.destroy();
+        pageScene.destroy();
+        controller.destroy();
+      },
+    },
+    {
+      namespace: 'fashion',
+      beforeEnter() {
+        logo.href = '../index.html';
+        gsap.fromTo(
+          '.nav-header',
+          1,
+          { y: '100%' },
+          { y: '0%', ease: 'power2.inOut' }
+        );
+      },
+    },
+  ],
+  transitions: [
+    {
+      leave({ current, next }) {
+        let done = this.async();
+
+        const tl = gsap.timeline({ defaults: { ease: 'power2.inOut' } });
+        tl.fromTo(current.container, 1, { opacity: 1 }, { opacity: 0 });
+        tl.fromTo(
+          '.swipe',
+          0.75,
+          { x: '-100%' },
+          { x: '0%', onComplete: done },
+          '-=0.5'
+        );
+      },
+      enter({ current, next }) {
+        let done = this.async();
+
+        window.scrollTo(0, 0);
+
+        const tl = gsap.timeline({ defaults: { ease: 'power2.inOut' } });
+        tl.fromTo(
+          '.swipe',
+          1,
+          { x: '0%' },
+          { x: '100%', stagger: 0.25, onComplete: done }
+        );
+        tl.fromTo(next.container, 1, { opacity: 0 }, { opacity: 1 });
+      },
+    },
+  ],
+});
+
 //EventListeners
 burger.addEventListener('click', navToggle);
 window.addEventListener('mousemove', cursor);
 window.addEventListener('mouseover', activeCursor);
-
-animateSlides();
